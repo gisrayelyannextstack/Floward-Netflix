71225034fda491d17a58813a750cb44276cc6cc3
gisrayelyannextstack <gerasimisrayelyan@nextstack.org>
2022-11-13 22:19:22 +0400

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Gerasim Israyelyan on 13.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
